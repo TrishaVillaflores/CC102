0cdfea3b266d4cad8d290261f6f23913677c8535
TrishaVillaflores <villaflorestrisha16@gmail.com>
2024-10-03 09:20:21 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Our Lady of Lourdes College Foundation" << endl;
+	cout << "College of Computer Studies" << endl;
+	cout << "BSIT 1-A" << endl;
+	cout << "Trisha Ane C. Villaflores" << endl;
+	cout << "Purok-2 Brgy Sta.Cruz Labo Camarines Norte" << endl;
+	return 0;
+}
